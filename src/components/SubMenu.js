e5d149319e601f7a0aa015a10182dccf7b654144
Daniel Sammy - Space Informática <71520454+Danielsammy@users.noreply.github.com>
2021-05-18 17:59:16 -0300

    Update SubMenu.js

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native';
-
+import { View, Text, StyleSheet, TouchableWithoutFeedback, Animated } from 'react-native';
+import {AntDesign, Entypo} from '@expo/vector-icons'
 // import { Container } from './styles';
 
 export default class SubMenu extends Component {
@@ -8,7 +8,9 @@ export default class SubMenu extends Component {
         return (
             <View style={[styles.container, this.props.style]}>
             <TouchableWithoutFeedback>
-                <Text>BOTAO</Text>
+            <Animated.View style={[styles.button, styles.menu]}>
+            <AntDesign name="plus" size={24} color="#FFF"/>
+            </Animated.View>
             </TouchableWithoutFeedback>
             </View>
 
@@ -21,5 +23,22 @@ const styles = StyleSheet.create({
     container:{
       alignItems:'center',
       position: 'absolute'
+    },
+    button:{
+        position:'absolute',
+        width: 60,
+        height: 60,
+        borderRadius: 60/2,
+        justifyContent: 'center',
+        alignItems:'center',
+        shadowRadius: 10,
+        shadowColor: '#D3D3D3',
+        shadowOpacity: 0.3,
+        shadowOffset: {
+            height: 10,
+        }
+    },
+    menu:{
+        backgroundColor:'#D3D3D3'
     }
   });
