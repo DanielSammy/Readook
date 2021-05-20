6ac9670e7ae0decbbabcf3aafd812a14bc822f71
Otávio Guimarães Silva <70302129+otavioguisilva@users.noreply.github.com>
2021-05-20 17:50:47 -0300

    Update Chat.js
    chat

--- src/components/screens/Chat.js
+++ src/components/screens/Chat.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { Avatar, Card, TextInput, Button, Text } from 'react-native-paper'
 import { telaChat } from '../Estilo'
@@ -6,44 +6,41 @@ import { theme } from '../PageStyle'
 import { GiftedChat } from 'react-native-gifted-chat'
 
 const Chat = (props) => {
-  const teste = () => {
-    fetch('http://192.168.0.27:8082/usuarios')
-    .then(response => response.json())
-    .then(users => console.warn(users))
-  }
   const user1 = {
     _id: 1,
     name: "Otavio",
-    avatar: "url"
+    avatar: 'https://drive.google.com/file/d/1VOXiB1MQ9SGGy7GGcNoUPWdl49d4uvf6/view?usp=sharing'
   }
 
   const user2 = {
     _id: 2,
     name: "Daniel",
-    avatar: "url"
+    avatar: 'https://randomuser.me/api/portraits/men/92.jpg'
   }
 
-  const messages = [
-    {
-      _id: 1,
-      text: "Boa Tarde",
-      createdAt: new Date(),
-      user : {user2},
-    },
-    {
-      _id: 2,
-      text: "Opa Fala ai Cara",
-      createdAt: new Date,
-      user: {user1},
-    },
-  ]
+  const [ messages, setMessages ] = useState([]);
 
-  function onSendMessage(message) {
-    console.log(message)
-  }
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Boa Noite, Como vai?',
+        createdAt: new Date(Date.UTC(2021, 5, 20, 20, 10, 0)),
+        user: {
+          _id:2,
+          name:'Otavio',
+          avatar: 'https://randomuser.me/api/portraits/men/26.jpg'
+        },
+      },
+    ])
+  },[])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
 
   return (
-    <GiftedChat user={user1} messages={messages} onSend={teste}/>
+    <GiftedChat user={user1} messages={messages} onSend={onSend}/>
   )
 }
 
