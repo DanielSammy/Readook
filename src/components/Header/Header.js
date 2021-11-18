4ce21bc99b46f94a209a86e770d53335beaf78b5
Daniel Sammy <dandan1702@gmail.com>
2021-11-18 11:03:07 -0300

    Color Appbar
    Correção tela principal:
    
    posicionamento dos menus

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { Container, Top, Logo, Title } from './style'
 import {StyleSheet} from 'react-native'
 import logo from '../Img/logo3x.png'
-import { theme } from '../PageStyle'
 import { Appbar } from 'react-native-paper'
 
 export default function Header() {
 return(
     <Container>
-        <Appbar.Action icon="dots-vertical" onPress={() => {}} />
+        <Appbar.Action icon="dots-vertical" color= '#fff' onPress={() => {}} />
         <Top>
         <Title style={styles.font}>Readook</Title>
         </Top>
